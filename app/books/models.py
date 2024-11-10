64e1b3af40348a38eb8a1a03efb543497d5c66f6
Bastian Katzmann <bastian.katzmann@web.de>
2024-11-10 17:53:13 +0100

    added model

--- app/books/models.py
+++ app/books/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Shelf(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100, blank=True)
+    isbn = models.CharField(max_length=13, blank=True, unique=True)
+    description = models.TextField(blank=True)
+    published_date = models.DateField(null=True, blank=True)
+    added_date = models.DateField(auto_now_add=True)
+    cover_image = models.ImageField(upload_to='book_covers/', blank=True)
+    shelf = models.ForeignKey(Shelf, on_delete=models.CASCADE, null=True, blank=True)
+    
+    def __str__(self):
+        return self.title
